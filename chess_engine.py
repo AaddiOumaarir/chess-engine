e288d2e0ff921a1ceb9b9e98f2a181bc006d8d80
AaddiOumaarir <aaddioumaarir44@gmail.com>
2024-11-04 22:31:54 +0100

    simple-chess-engine

---
+++ chess_engine.py
@@ -0,0 +1,14 @@
+import numpy as np
+class GameState:
+    # '--' represent empty square
+    def __init__(self):
+        self.board = np.array([['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+                               ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+                               ['--', '--', '--', '--', '--', '--', '--', '--'],
+                               ['--', '--', '--', '--', '--', '--', '--', '--'],
+                               ['--', '--', '--', '--', '--', '--', '--', '--'],
+                               ['--', '--', '--', '--', '--', '--', '--', '--'],
+                               ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+                               ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']])
+        self.WhiteToMove = True
+        self.moveLog = []
--- main.py
+++ main.py
@@ -1,3 +1,50 @@
-def test():
-    print("I am still learning Github")
-    return False
+import pygame as p
+
+
+import chess_engine
+
+HEIGHT = WIDTH = 400
+DIMENTION = 8
+SQUARE_SIZE = HEIGHT//DIMENTION
+MAX_FPS = 15
+Images = {}
+
+def load_images():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        Images[piece] = p.transform.scale(p.image.load('chess_pieces/'+piece+'.png'), (SQUARE_SIZE,SQUARE_SIZE))
+
+def main():
+    p.init()
+    screen  = p.display.set_mode((WIDTH, HEIGHT))
+
+    clock = p.time.Clock()
+    screen.fill(p.Color('white'))
+    game_state = chess_engine.GameState()
+    load_images()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        draw_game_state(screen, game_state)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def draw_game_state(screen, game_state):
+    draw_board(screen)
+
+    draw_pieces(screen, game_state.board)
+
+def draw_board(screen):
+    colors = [p.Color('gold'), p.Color('gray')]
+    for r in range(DIMENTION):
+        for c in range(DIMENTION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+def draw_pieces(screen, board):
+    pass
+
+
+
+main()
